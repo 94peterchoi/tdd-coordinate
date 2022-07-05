ac4cb6704a0eafb2417c116f7581248539ac0c87
csw <csw@elonsoft.co.kr>
2022-07-05 22:19:16 +0900

    메서드명, 람다식 리팩토링

--- src/main/java/Coordinate.java
+++ src/main/java/Coordinate.java
@@ -5,23 +5,23 @@ public class Coordinate {
 
     List<Pointer> pointers = new ArrayList<>();
 
-    public void addTwoPointers(String twoNums) {
+    public void addPointers(String twoNums) {
         if (pointers.size() > 1) {
             throw new IllegalStateException("이미 저장된 좌표가 존재합니다.");
         }
 
         String[] pointerArr = twoNums.split("-");
-        Pointer pointer1 = makePointer(pointerArr[0]);
-        Pointer pointer2 = makePointer(pointerArr[1]);
 
-        pointers.add(pointer1);
-        pointers.add(pointer2);
+        for (String strPointer : pointerArr) {
+            Pointer pointer = createPointer(strPointer);
+            pointers.add(pointer);
+        }
     }
 
-    public Pointer makePointer(String strNum) {
+    public Pointer createPointer(String strNum) {
         String[] numArr = strNum.split(",");
 
-        // 여기서 트캐를 해야하나
+        // 여기서 트캐를 해야하나 (암튼 예외처리가 필요함)
         int x = Util.strToInt(numArr[0]);
         int y = Util.strToInt(numArr[1]);
 
@@ -33,12 +33,11 @@ public class Coordinate {
         Pointer p2 = pointers.get(1);
 
         int[] posDiffs = p1.subtract(p2);
-
         return Math.sqrt(Math.pow(posDiffs[0], 2) + Math.pow(posDiffs[1], 2));
     }
 
     public boolean isPositionRightPlace(int x, int y) {
-        return pointers.stream().anyMatch((pointer) -> {return pointer.equals(new Pointer(x, y));});
+        return pointers.stream().anyMatch((pointer) -> pointer.equals(new Pointer(x, y)));
     }
 
     @Override
--- src/test/java/CoordinateTest.java
+++ src/test/java/CoordinateTest.java
@@ -13,15 +13,15 @@ public class CoordinateTest {
     @Test
     @DisplayName("x,y 좌표를 입력받는다")
     void getCoordinate() {
-        String twoNums = "10,10";
-        String[] numArr = twoNums.split(",");
+        String strPtr = "10,10";
+        String[] numArr = strPtr.split(",");
 
         assertThat(numArr.length).isEqualTo(2);
         assertThat(Util.strToInt(numArr[0])).isEqualTo(10);
         assertThat(Util.strToInt(numArr[1])).isEqualTo(10);
 
-        twoNums = "(10,10)";
-        numArr = twoNums.split(",");
+        strPtr = "(10,10)";
+        numArr = strPtr.split(",");
 
         int x = Util.strToInt(numArr[0]);
         int y = Util.strToInt(numArr[1]);
@@ -35,16 +35,16 @@ public class CoordinateTest {
     @Test
     @DisplayName("입력받은 문자열을 가지고 좌표 2개를 만든다")
     void getTwoCoordinates() {
-        String twoNums = "(10,10)-(1,4)";
+        String strPtrs = "(10,10)-(1,4)";
 
         Coordinate coordinate = new Coordinate();
-        coordinate.addTwoPointers(twoNums);
+        coordinate.addPointers(strPtrs);
         assertThat(coordinate.toString()).isEqualTo("x:10, y:10 / x:1, y:4");
 
-        twoNums = "(5,6)-(7,8)";
+        strPtrs = "(5,6)-(7,8)";
 
         Coordinate coordinate2 = new Coordinate();
-        coordinate2.addTwoPointers(twoNums);
+        coordinate2.addPointers(strPtrs);
         assertThat(coordinate2.toString()).isEqualTo("x:5, y:6 / x:7, y:8");
     }
 
@@ -52,17 +52,17 @@ public class CoordinateTest {
     @Test
     @DisplayName("두 좌표 사이의 거리를 구한다")
     void getDistanceBetweenTwoPointers() {
-        String twoNums = "(3,4)-(3,4)";
+        String strPtrs = "(3,4)-(3,4)";
         Coordinate coordinate = new Coordinate();
-        coordinate.addTwoPointers(twoNums);
+        coordinate.addPointers(strPtrs);
         double distance = coordinate.getDistanceBetweenTwoPointers();
 
         assertThat(distance).isEqualTo(0, offset(0.09));
 
 
-        twoNums = "(10,10)-(14,15)";
+        strPtrs = "(10,10)-(14,15)";
         Coordinate coordinate2 = new Coordinate();
-        coordinate2.addTwoPointers(twoNums);
+        coordinate2.addPointers(strPtrs);
         distance = coordinate2.getDistanceBetweenTwoPointers();
 
         assertThat(distance).isEqualTo(6.403124, offset(0.09));
@@ -80,9 +80,9 @@ public class CoordinateTest {
 //        pointers.add(new Pointer(9, 9));
 //        pointers.add(new Pointer(17, 20));
 
-        String twoNums = "(10,10)-(14,15)";
+        String strPtrs = "(10,10)-(14,15)";
         Coordinate coordinate = new Coordinate();
-        coordinate.addTwoPointers(twoNums);
+        coordinate.addPointers(strPtrs);
 
         ResultView.printPointers(coordinate);
         System.out.println(coordinate.getDistanceBetweenTwoPointers());
@@ -90,13 +90,12 @@ public class CoordinateTest {
         System.out.println();
         System.out.println();
 
-        twoNums = "(1,1)-(17,20)";
+        strPtrs = "(1,1)-(17,20)";
         Coordinate coordinate2 = new Coordinate();
-        coordinate2.addTwoPointers(twoNums);
+        coordinate2.addPointers(strPtrs);
 
         ResultView.printPointers(coordinate2);
         System.out.println(coordinate2.getDistanceBetweenTwoPointers());
-
 
     }
 
